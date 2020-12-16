9d6d59c84d08d73b1e317ed68ff4bb6abb422866
jingliancui <ericcui@qq.com>
2020-12-16 17:25:12 +0800

    Add arch sample project.

---
+++ Android/ArchSample/SampleApp/SampleApp/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Com.Qmuiteam.Qmui.Arch;
+
+namespace SampleApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyQMTheme", MainLauncher = true)]
+    public class MainActivity : QMUIActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            QMUISwipeBackActivityManager.Init(Application);
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var btn = FindViewById<Button>(Resource.Id.NaviBtn);
+            btn.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Android.Content.Intent(this, typeof(SecondActivity));
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
